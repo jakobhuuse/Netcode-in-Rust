41fe72c78696e7f39deba0677436321d0b77c3fb
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-21 13:43:58 +0200

    feat: setup svelte ui

---
+++ demo/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Header from '$lib/components/layout/Header.svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<Header></Header>
+{@render children()}
